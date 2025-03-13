fc8aa20f85b25721066feeb08841d7ae27e38500
kalyan alladi <55277793+kalyan12345678@users.noreply.github.com>
2025-03-13 20:03:41 +0530

    Update Chained Promises for Order Processing.js

--- Chained Promises for Order Processing.js
+++ Chained Promises for Order Processing.js
@@ -1,14 +1,14 @@
 
-function placeOrder() {
+function placeOrder() { 
     return new Promise((resolve, reject) => {
-      const orderId = Math.floor(Math.random() * 90) + 10
+      let orderId = Math.floor(Math.random() * 90) + 10
       console.log(`Order ID: ${orderId}`)
       resolve(orderId)
     })
   }
   function processOrder(orderId) {
     return new Promise((resolve, reject) => {
-      const processingCode = Math.floor(Math.random() * 90) + 10
+      let processingCode = Math.floor(Math.random() * 90) + 10
       console.log(`Processing Code: ${processingCode}`)
       resolve(processingCode)
     })
@@ -17,7 +17,7 @@ function placeOrder() {
   
   function confirmOrder(processingCode) {
     return new Promise((resolve, reject) => {
-      const confirmationNumber = Math.floor(Math.random() * 90) + 10
+      let confirmationNumber = Math.floor(Math.random() * 90) + 10
       console.log(`Confirmation Number: ${confirmationNumber}`)
       resolve(confirmationNumber)
     })
@@ -25,9 +25,9 @@ function placeOrder() {
   
   function calculateFinalResult(confirmationNumber) {
     return new Promise((resolve, reject) => {
-      const extraNumber = Math.floor(Math.random() * 90) + 10
+      let extraNumber = Math.floor(Math.random() * 90) + 10
       console.log(`Extra Number: ${extraNumber}`)
-      const finalResult = confirmationNumber + extraNumber
+      let finalResult = confirmationNumber + extraNumber
       resolve(finalResult)
     })
   }
